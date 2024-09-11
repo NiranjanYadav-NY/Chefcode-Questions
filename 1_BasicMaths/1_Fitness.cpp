ce0bc008db5254e30157093b5cecc686fde05bea
NiranjanYadav_NY <neirandranjan@gmail.com>
2024-09-11 18:44:00 +0530

    Add files via upload

---
+++ 1_BasicMaths/1_Fitness.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x, y;
+        cin>>x;
+        // your code goes here
+        y = 5*2*x;
+        cout<<y<<endl;
+    }
+}
